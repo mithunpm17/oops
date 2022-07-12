6695cb5f6565ede661704927fc631efaf391e5ea
mithunpm17 <103182136+mithunpm17@users.noreply.github.com>
2022-07-12 20:46:55 +0530

    Add files via upload

--- matrix.java
+++ matrix.java
@@ -1,18 +1,23 @@
-public class matrix
-{
-public static void main(String args[])
-{
-int a[][]={{1,3,4},{2,4,3},{3,4,5}};
-int b[][]={{1,3,4},{2,4,3},{1,2,4}};
-int c[][]=new int[3][3];
-for(int i=0;i<3;i++)
-{
-for(int j=0;j<3;j++)
-{
-c[i][j]=a[i][j]+b[i][j];
-System.out.print(c[i][j]+" ");
+public class matrix{
+	public static void main(String[] args) {
+		
+		int a[][] = {{3,4,5},
+		             {5,3,7},
+		             {4,5,7}},
+		    b[][]={{3,4,5},
+		          {5,6,7},
+		          {7,8,9}};
+
+		int c[][] = new int[3][3];
+
+		for(int i =0;i<3;i++){
+			for(int j=0;j<3;j++){
+
+				c[i][j] = a[i][j] + b[i][j];
+				System.out.print(c[i][j] +" ");
+			}
+
+			System.out.println();
+		}
+	}
 }
-System.out.println();
-}
-}
-} 
